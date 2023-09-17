e173867df08977412c90cdcc65a4f41a1d7d5c86
edsonlcandido <edson.luiz.candido@gmail.com>
2023-09-17 20:01:00 -0300

    tentativa de corrigir o erro websocket no container

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.WebSockets;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
+builder.Services.AddServerSideBlazor()
+    .AddHubOptions(o => {
+        o.ClientTimeoutInterval = TimeSpan.FromSeconds(30);
+        o.HandshakeTimeout = TimeSpan.FromSeconds(30);
+        });
 
 var app = builder.Build();
-
 
 app.UseStaticFiles();
 
@@ -15,5 +20,7 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.UseWebSockets();
 
 app.Run();
